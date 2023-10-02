ff68ef49f741683f8298e3054bd2501965a17fc4
pavere <kostrab@icloud.com>
2023-10-02 17:15:32 +0200

    Evidencia vydavkov v1.1

--- src/main/java/sk/tmconsulting/evidenciavydavkov/view/GUI.java
+++ src/main/java/sk/tmconsulting/evidenciavydavkov/view/GUI.java
@@ -11,23 +11,27 @@ import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import com.toedter.calendar.JDateChooser;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
 
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 
 
 public class GUI {
+    private static int indexVydavku;
     public static void main(String[] args) {
 
         JFrame hlavneOkno = new JFrame("Evidencia výdavkov"); // vytvorime okno
@@ -116,7 +120,7 @@ public class GUI {
         JComboBox cmbKategorie = new JComboBox(kategorie);
         cmbKategorie.setFont(new Font("Montserrat", Font.PLAIN, 12));
         cmbKategorie.setSelectedIndex(4);
-        cmbKategorie.setBounds(190,120,200,30); // x, y, sirka, vyska
+        cmbKategorie.setBounds(190,120,203,30); // x, y, sirka, vyska
         backgroundPanel.add(cmbKategorie);
 
         // Date picker- Datum
@@ -129,13 +133,13 @@ public class GUI {
         backgroundPanel.add(labelDatum);
 
         // date picker  -vyberač dátumu OLD ONE
-       /* UtilDateModel model = new UtilDateModel();
+        UtilDateModel model = new UtilDateModel();
         JDatePanelImpl datePanel = new JDatePanelImpl(model);
         JDatePickerImpl datePicker = new JDatePickerImpl(datePanel);
         datePicker.setBounds(190,150,200,30); // x, y, sirka, vyska
         JFormattedTextField textField = datePicker.getJFormattedTextField();
         textField.setFont(new Font("sans-serif", Font.BOLD, 13));
-        panel.add(datePicker);*/
+        hlavneOkno.add(datePicker);
         //date picker  -vyberač dátumu OLD END
 
        /* JCalendar calendar = new JCalendar(); // Vytvorte inštanciu JCalendar
@@ -144,11 +148,11 @@ public class GUI {
 
         hlavneOkno.setVisible(true);*/
         // jCalendar picker NEW
-        JDateChooser dateChooser = new JDateChooser(); // Vytvorte inštanciu JDateChooser
+        /*JDateChooser dateChooser = new JDateChooser(); // Vytvorte inštanciu JDateChooser
 
 
 
-        dateChooser.setBounds(190,150,200,30); // x, y, sirka, vyska
+        dateChooser.setBounds(195,150,193,30); // x, y, sirka, vyska
         //dateChooser.getJCalendar().setPreferredSize(new Dimension(450, 300));
 
 
@@ -160,16 +164,130 @@ public class GUI {
          //dateChooser.setDateFormatString("dd.MM.yyyy HH:mm");
 
          URL iconURL = dateChooser.getClass().getResource(
-         "/com/toedter/calendar/images/JDayChooserColor32.gif");
+         "/com/toedter/calendar/images/JDayChooserColor16.gif");
          ImageIcon icon = new ImageIcon(iconURL);
-         dateChooser.setIcon(icon);
+         dateChooser.setIcon(icon);*/
 
 
-        hlavneOkno.add(dateChooser);
+        hlavneOkno.add(datePicker);
 
 
 
 
+        // Zoznam vydavkov label
+        JLabel labelZoznamVydavkov = new JLabel("Zoznam výdavkov");
+        labelZoznamVydavkov.setBounds(385, 35, 150, 20); // x, y, sirka, vyska
+        //labelCenaVydavku.setHorizontalAlignment(JTextField.RIGHT);
+        labelZoznamVydavkov.setFont(new Font("Montserrat", Font.BOLD, 12));
+        labelZoznamVydavkov.setHorizontalAlignment(SwingConstants.RIGHT); // text centrovanie vpravo
+        hlavneOkno.add(labelZoznamVydavkov);
+
+        // Zoznam vydavkov list
+        //JList listZoznamVydavkov = new JList();
+       // listZoznamVydavkov.setBounds(420, 65,330, 300);
+        //hlavneOkno.add(listZoznamVydavkov);
+        DefaultListModel modelZoznamu = new DefaultListModel<>();
+        JList listZoznamVydavkov = new JList<>(modelZoznamu);
+        //Priadenie udajov na testovanie
+        modelZoznamu.addElement("Chlieb 2.3 POTRAVINY 27.9.2023");
+        modelZoznamu.addElement("Rožky 1.3 POTRAVINY 23.9.2023");
+        listZoznamVydavkov.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if(!e.getValueIsAdjusting()){
+                try{
+
+                String vybranyVydavok = listZoznamVydavkov.getSelectedValue().toString();
+                //String jednotliveUdajeVydavku [] = vybranyVydavok.split(" ");
+
+                    //String jednotliveUdajeVydavku [] = vybranyVydavok.split(" ");
+                    String regex = " ";
+                    String jednotliveUdajeVydavku [] = vybranyVydavok.split(regex);
+
+                    textNazovVydavku.setText(jednotliveUdajeVydavku[0]);
+                    textCenaVydavku.setText(jednotliveUdajeVydavku[1]);
+                    cmbKategorie.setSelectedItem(jednotliveUdajeVydavku[2]);
+
+                    datePicker.getJFormattedTextField().setText(jednotliveUdajeVydavku[3]);
+
+                    indexVydavku = listZoznamVydavkov.getSelectedIndex();
+                    System.out.println(Arrays.toString(jednotliveUdajeVydavku));
+                    System.out.println(vybranyVydavok);
+
+                    System.out.println(Arrays.toString(jednotliveUdajeVydavku)); // vypise jednotlive prvky pola
+                System.out.println(vybranyVydavok);
+                } catch (NullPointerException e1){
+                    //TODO spracovat
+                }
+                }
+                }
+
+        });
+
+
+        // Zoznam vydavkov scrollbar
+        JScrollPane scbZoznamVydavkov = new JScrollPane(listZoznamVydavkov);
+        scbZoznamVydavkov.setBounds(420,65,330,300);
+        hlavneOkno.add(scbZoznamVydavkov);
+
+        //Tlacidlo na pridanie zaznamu
+        JButton btnPridajZaznam = new JButton("Pridaj záznam");
+        btnPridajZaznam.setBounds(85, 405,150, 30);
+        btnPridajZaznam.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO po kliknuti na tlacidlo musime doporgramovat
+               /* SimpleDateFormat dcn = new SimpleDateFormat("dd.MM.yyyy");
+                String date = dcn.format(dateChooser.getDate());*/
+                //Potom by sa to mohlo takto vlozit do modelu:
+                // date.toString();
+
+                /*modelZoznamu.addElement("Názov: " + textNazovVydavku.getText() + "Cena: " +  " " +  textCenaVydavku.getText() + " " + cmbKategorie.getSelectedItem() + " " + date);*/
+                modelZoznamu.addElement(textNazovVydavku.getText() + " " +  textCenaVydavku.getText() + " " + cmbKategorie.getSelectedItem() +  datePicker.getJFormattedTextField().getText().replace(" ", ""));
+            }
+        });
+        hlavneOkno.add(btnPridajZaznam);
+
+
+//Tlacidlo na upravu zaznamu
+        JButton btnUpravZaznam = new JButton("Upraviť záznam");
+        btnUpravZaznam.setBounds(225, 405,150, 30);
+        btnUpravZaznam.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO po kliknuti na tlacidlo musime doporgramovat
+                modelZoznamu.setElementAt(textNazovVydavku.getText() + " " +  textCenaVydavku.getText() + " " + cmbKategorie.getSelectedItem() +  datePicker.getJFormattedTextField().getText().replace(" ", ""), indexVydavku);
+            }
+        });
+        hlavneOkno.add(btnUpravZaznam);
+
+
+        //Tlacidlo na odstranit zaznamu
+        JButton btnOdstranZaznam = new JButton("Odstrániť záznam");
+        btnOdstranZaznam.setBounds(365, 405,150, 30);
+        btnOdstranZaznam.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO po kliknuti na tlacidlo musime doporgramovat
+
+                modelZoznamu.remove(indexVydavku);
+
+            }
+        });
+        hlavneOkno.add(btnOdstranZaznam);
+
+
+        //Tlacidlo na ukoncenie
+        JButton btnUkonciAplikaciu = new JButton("Ukončiť");
+        btnUkonciAplikaciu.setBounds(505, 405,150, 30);
+        btnUkonciAplikaciu.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO po kliknuti na tlacidlo musime doporgramovat
+                System.exit(0);
+            }
+        });
+        hlavneOkno.add(btnUkonciAplikaciu);
 
 
        /* JButton btnOk = new JButton("Vypiš!"); // tlacidlo
